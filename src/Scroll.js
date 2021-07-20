2d095655acbc65913acda8f9e2a091885fd27110
K95Monty <48034846+K95Monty@users.noreply.github.com>
2021-07-20 19:49:50 +0100

    Add files via upload
    made responsive 
    fixed ox shadows
    added new scroll-to-top function

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaArrowAltCircleUp } from 'react-icons/fa';
 import './Scroll.css';
 
 function Scroll() {
+    const [showScroll, setShowScroll] = useState(false)
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset <= 400){
+        setShowScroll(true)
+    } 
+    else if (showScroll && window.pageYOffset > 400){
+        setShowScroll(false)
+    }
+  };
+
+    const scrollTop = () =>{
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    };
+
+    window.addEventListener('scroll', checkScrollTop)
+  
     return (
-        <div className="scroll">
-            <FaArrowAltCircleUp className="scroll-icon"/>
-        </div>
+            <div className={ showScroll ? 'scroll-hide' : 'scroll-active'}>
+                <FaArrowAltCircleUp className="scroll-icon"
+                    onClick={scrollTop}/>
+            </div>
     )
 }
 
